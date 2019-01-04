7ca6d916aa4424d2026ff113e3ff2ae69b7ddaf0
nadeeraka <nimanthanadeeraka@outlook.com>
2019-01-04 16:05:54 +0530

    Delete dbconfig.js

--- config/db/dbconfig.js
+++
@@ -1,9 +0,0 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    mongoURI: "mongodb://nick:TzCN8KHfJd6MvAa@ds257640.mlab.com:57640/vidjot"
-  };
-} else {
-  module.exports = {
-    mongoURI: "mongodb://localhost/vidjot"
-  };
-}
