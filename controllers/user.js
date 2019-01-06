6c1ad5175c57a5f71578a69055eabe1c26f0eb06
nadeeraka <nimanthanadeeraka@outlook.com>
2019-01-06 20:58:28 +0530

    validation 3  added

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,30 @@
 const ideas = require("../models/user");
 const flash = require("connect-flash");
+const { validationResult } = require("express-validator/check");
 
 // register user
-router.post("/user/register", (req, res) => {
+exports.postReg = ("/user/register",
+(req, res) => {
   let errors = [];
+  if (!(req.body.password === req.body.cPassword)) {
+    errors.push({ text: "password dose not match" });
+  }
 
-  if (req.body.name.length < 3) {
-    errors.push({ text: "Invalid user name" });
+  const validationError = validationResult(req);
+  if (!validationError.isEmpty()) {
+    console.log(validationError);
+    errors.push({ text: validationError.array()[0].msg });
+  }
+  if (errors.length > 0) {
+    res.render("api/reg", {
+      title: "Register",
+      e: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      cPassword: req.body.cPassword
+    });
+  } else {
+    render("/idea");
   }
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 
-const { check } = require("express-validator/check");
+const { check, body } = require("express-validator/check");
 // to import controller
 const controller = require("../controllers/controller");
+const userController = require("../controllers/user");
 router.get("/", (req, res) => {
   res.render("index", { title: "Luba" });
 });
@@ -47,10 +48,24 @@ router.get("/user/login", (req, res) => {
 router.get("/user/register", (req, res) => {
   res.render("api/reg", { title: "Register" });
 });
-router.post("/user/register", (req, res) => {
-  console.log(req.body);
-  res.render("api/login", { title: "Register" });
-});
+router.post(
+  "/user/register",
+  body("name")
+    .isAlpha()
+    .withMessage("Please use letters")
+    .isLength({ max: 12, min: 3 })
+    .withMessage("Invalid Length"),
+  body("email")
+    .isEmail()
+    .withMessage("Invalid email"),
+  body("password")
+    .isLength({ max: 24, min: 4 })
+    .withMessage("Invalid Length"),
+  body("cPassword")
+    .isLength({ max: 24, min: 4 })
+    .withMessage("Invalid Length"),
+  userController.postReg
+);
 
 router.use((req, res) => {
   res.render("404", { title: "Error" });
